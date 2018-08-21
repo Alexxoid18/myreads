e0ec0ca6ab02465a248408ce9bbf54a6a61cc0a6
Алексей Николаевич Ружников <RuzhnikovAN@meria.arhcity.ru>
2018-08-21 16:44:21 +0300

    Add 'Main' component

--- src/Search.js
+++ src/Search.js
@@ -4,8 +4,24 @@ import Book from './Book'
 class Search extends Component {
 	render() {
 		return (
-			<div>
-			</div>
+			<div className="search-books">
+            <div className="search-books-bar">
+              
+              <a className="close-search" 
+              onClick={() => this.setState({ showSearchPage: 
+                false })}>Close
+              </a>
+             
+              <div className="search-books-input-wrapper">
+                
+              <input type="text" placeholder="Search by title or author"/>
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
 		)
 
 	}
